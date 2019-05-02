d37d4216050056f710ba9b8546595930e73ea27d
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2019-05-02 15:29:17 +0430

    Navigate to FragmentB whenever user clicked on login button

--- app/src/main/java/com/phelat/navigationresultsample/FragmentA.kt
+++ app/src/main/java/com/phelat/navigationresultsample/FragmentA.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_a.*
 
 class FragmentA : Fragment() {
 
@@ -13,5 +15,10 @@ class FragmentA : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? = inflater.inflate(R.layout.fragment_a, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        loginButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.a_to_b))
+    }
 
 }
---
+++ app/src/main/java/com/phelat/navigationresultsample/FragmentB.kt
@@ -0,0 +1,17 @@
+package com.phelat.navigationresultsample
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FragmentB : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_b, container, false)
+
+}
