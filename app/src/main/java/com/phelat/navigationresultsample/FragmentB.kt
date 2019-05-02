9ecaf6dd8a02831e1fe7b4a0fc3013723a58d50b
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2019-05-02 15:43:25 +0430

    Validate username and password, if correct navigateUp

--- app/src/main/java/com/phelat/navigationresultsample/FragmentB.kt
+++ app/src/main/java/com/phelat/navigationresultsample/FragmentB.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.fragment_b.*
 
 class FragmentB : Fragment() {
 
@@ -13,5 +16,28 @@ class FragmentB : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? = inflater.inflate(R.layout.fragment_b, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        loginButton.setOnClickListener(::onLoginButtonClicked)
+    }
+
+    private fun onLoginButtonClicked(view: View) {
+        if (isUsernameAndPasswordValid()) {
+            findNavController().navigateUp()
+        } else {
+            Snackbar.make(
+                view,
+                R.string.general_wrong_login_credentials_message,
+                Snackbar.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    private fun isUsernameAndPasswordValid(): Boolean {
+        val username = usernameInput.text.toString()
+        val password = passwordInput.text.toString()
+        return username == "admin" && password == "1234"
+    }
 
 }
