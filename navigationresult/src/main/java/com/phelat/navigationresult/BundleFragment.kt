7db38a9288fdb16a859c61cbdde1e3290173ec75
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2019-05-11 01:17:57 +0430

    Reflect all the possible navigate overloads with direction id

--- navigationresult/src/main/java/com/phelat/navigationresult/BundleFragment.kt
+++ navigationresult/src/main/java/com/phelat/navigationresult/BundleFragment.kt
@@ -4,29 +4,57 @@ import android.os.Bundle
 import androidx.annotation.IdRes
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavDirections
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigator
 import androidx.navigation.fragment.findNavController
 
 abstract class BundleFragment : Fragment() {
 
-    var pendingRequest: Int = -1
+    internal var pendingRequest: Int = -1
 
     open fun onFragmentResult(requestCode: Int, bundle: Bundle) {}
 
     fun navigate(navDirection: NavDirections, requestCode: Int = -1) {
-        pendingRequest = if (requestCode > -1) {
-            navDirection.arguments.putInt(Constants.FRAGMENT_REQUEST_CODE, requestCode)
-            requestCode
-        } else {
-            navDirection.arguments.getInt(Constants.FRAGMENT_REQUEST_CODE, -1)
-        }
+        extractPendingRequest(navDirection.arguments, requestCode)
         findNavController().navigate(navDirection)
     }
 
     fun navigate(@IdRes navDirection: Int, requestCode: Int) {
-        pendingRequest = requestCode
-        findNavController().navigate(navDirection, Bundle().apply {
-            putInt(Constants.FRAGMENT_REQUEST_CODE, requestCode)
-        })
+        navigate(navDirection, null, requestCode)
+    }
+
+    fun navigate(@IdRes navDirection: Int, bundle: Bundle?, requestCode: Int) {
+        navigate(navDirection, bundle, null, requestCode)
+    }
+
+    fun navigate(
+        @IdRes navDirection: Int,
+        bundle: Bundle?,
+        navOptions: NavOptions?,
+        requestCode: Int
+    ) {
+        navigate(navDirection, bundle, navOptions, null, requestCode)
+    }
+
+    fun navigate(
+        @IdRes navDirection: Int,
+        bundle: Bundle?,
+        navOptions: NavOptions?,
+        navigatorExtras: Navigator.Extras?,
+        requestCode: Int
+    ) {
+        val theActualBundle = bundle ?: Bundle()
+        extractPendingRequest(theActualBundle, requestCode)
+        findNavController().navigate(navDirection, theActualBundle, navOptions, navigatorExtras)
+    }
+
+    private fun extractPendingRequest(bundle: Bundle, requestCode: Int) {
+        pendingRequest = if (requestCode > -1) {
+            bundle.putInt(Constants.FRAGMENT_REQUEST_CODE, requestCode)
+            requestCode
+        } else {
+            bundle.getInt(Constants.FRAGMENT_REQUEST_CODE, -1)
+        }
     }
 
 }
