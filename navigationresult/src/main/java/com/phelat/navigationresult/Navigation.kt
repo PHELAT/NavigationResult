5d669495931a2b2cf6fcb8f9c23fd584cb83b1ce
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2020-04-12 00:35:03 +0430

    Throw exception if activity isn't extended from FragmentResultActivity

--- navigationresult/src/main/java/com/phelat/navigationresult/Navigation.kt
+++ navigationresult/src/main/java/com/phelat/navigationresult/Navigation.kt
@@ -6,5 +6,6 @@ import androidx.navigation.fragment.findNavController
 
 fun Fragment.navigateUp(requestCode: Int, bundle: Bundle) {
     (activity as? FragmentResultActivity)?.setBundle(requestCode, bundle)
+        ?: throw IllegalStateException("You need to extend your activity from FragmentResultActivity")
     findNavController().navigateUp()
 }
